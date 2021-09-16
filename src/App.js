8ff364e85bc9827acb421eb8a58838bc43d1f290
zuev720 <zuev720mail.ru>
2021-09-16 13:34:46 +0300

    fix routes trying 1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import {
     BrowserRouter as Router,
     Switch,
-    Route, Redirect,
+    Route,
 } from "react-router-dom";
 import {MainPage} from './Components/MainPage/MainPage';
 import './App.css';
@@ -12,15 +12,14 @@ import {ErrorPage} from './Components/ErrorPage/ErrorPage';
 
 function App() {
     return (
-        <Router basename={'https://zuev720.github.io/ra-9.2'}>
+        <Router basename={'https://zuev720.github.io/ra-9.2/'}>
             <div className={'App'}>
                 <Switch>
-                    <Route path={'/posts/edit/:id'} component={EditPostPage}/>
-                    <Route path={'/posts/new'} component={PostCreatePage}/>
-                    <Route path={'/posts/:id'} component={(props) => <PostPage {...props}/>}/>
-                    <Route path={'/error'} component={ErrorPage}/>
-                    <Route exact path={'/ra-9.2'} component={MainPage}/>
-                    <Redirect to={'/ra-9.1'} />
+                    <Route path={'posts/edit/:id'} component={EditPostPage}/>
+                    <Route path={'posts/new'} component={PostCreatePage}/>
+                    <Route path={'posts/:id'} component={(props) => <PostPage {...props}/>}/>
+                    <Route path={'error'} component={ErrorPage}/>
+                    <Route exact path={'ra-9.2'} component={MainPage}/>
                 </Switch>
             </div>
         </Router>
