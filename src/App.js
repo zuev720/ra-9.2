06fb583ec664b1870bc645f4d55c3ef597449616
zuev720 <zuev720mail.ru>
2021-09-16 12:19:16 +0300

    Fixed baseName in routes

--- src/App.js
+++ src/App.js
@@ -12,14 +12,14 @@ import {ErrorPage} from './Components/ErrorPage/ErrorPage';
 
 function App() {
     return (
-        <Router basename={'/'}>
+        <Router basename={'https://zuev720.github.io/ra-9.2'}>
             <div className={'App'}>
                 <Switch>
                     <Route path={'/posts/edit/:id'} component={EditPostPage}/>
                     <Route path={'/posts/new'} component={PostCreatePage}/>
                     <Route path={'/posts/:id'} component={(props) => <PostPage {...props}/>}/>
                     <Route path={'/error'} component={ErrorPage}/>
-                    <Route exact component={MainPage}/>
+                    <Route exact path={'/ra-9.2'} component={MainPage}/>
                 </Switch>
             </div>
         </Router>
